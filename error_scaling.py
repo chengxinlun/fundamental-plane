19bfd01c1b58151dfea3e5d0a36ab42c53d5e8fb
chengxinlun <chengxinlun@gmail.com>
2016-04-02 01:56:45 +0800

    Finished error scaling

--- error_scaling.py
+++ error_scaling.py
@@ -1,6 +1,8 @@
 import pickle
 from position import Location
 from base import read_raw_data
+from functools import partial
+from multiprocessing import Manager, Pool
 
 
 def read_rcs(rmid):
@@ -11,14 +13,24 @@ def read_rcs(rmid):
     return rcs_dict
 
 
-def error_scaling_single(lock, rmid):
-    rcs_dict = read_rcs(rmid)
-    for each in rcs_dict.keys():
-        [wave, flux, error] = read_raw_data(rmid, int(each))
-        error = error * rcs_dict[each]
-        f = open(Location.project_loca + "data/raw/" + str(rmid) + "/" +
-                 str(each) + "/" + "error_scaled.pkl", "wb")
-        pickle.dump(error, f)
+def error_scaling_single(lock, rcs_dict, rmid, mjd):
+    [wave, flux, error] = read_raw_data(rmid, int(mjd))
+    error = error * rcs_dict[mjd]
+    f = open(Location.project_loca + "data/raw/" + str(rmid) + "/" +
+             str(mjd) + "/" + "error_scaled.pkl", "wb")
+    pickle.dump(error, f)
     lock.acquire()
-    print("Error scaling finished for " + str(rmid))
+    print("Error scaling finished for " + str(mjd))
     lock.release()
+
+
+def error_scaling(rmid):
+    rcs_dict = read_rcs(rmid)
+    p = Pool(processes = 32)
+    m = Manager()
+    l = m.Lock()
+    func = partial(error_scaling_single, l, rcs_dict, rmid)
+    p.map(func, rcs_dict.keys())
+    p.close()
+    p.join()
+    print("Finished for " + str(rmid) + "\n\n")
