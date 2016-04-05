ff5951f9c0b437efaff809a954ca2de9e8ce919c
Xinlun Cheng <chengxinlun@gmail.com>
2016-04-05 21:00:26 +0800

    FeII error estimation modified

--- error_estimate.py
+++ error_estimate.py
@@ -1,9 +1,12 @@
+import os
 import pickle
 import numpy as np
-from base import read_raw_data, mask_points, extract_fit_part, read_database, union
+from base import read_raw_data, mask_points, extract_fit_part, read_database,\
+    union
 from fe_temp_observed import FeII_template_obs
 from position import Location
 from functools import partial
+from multiprocessing import Manager, Pool
 
 
 def get_error(center, width, wave, flux, error):
@@ -19,13 +22,16 @@ def get_fe2_re(fit_res, wave, flux, error):
     width_n3 = fit_res[4] * np.sqrt(3 / 2) / 299792.458
     line_list = list()
     for each in FeII_template_obs.center_l1:
-        list.append(each - 2.0 * width_l1 * each, each + 2.0 * width_l1 * each)
+        line_list.append([each - 2.0 * width_l1 * each, each + 2.0 * width_l1 *
+                         each])
     for each in FeII_template_obs.center_n3:
-        list.append(each - 2.0 * width_n3 * each, each + 2.0 * width_n3 * each)
+        line_list.append([each - 2.0 * width_n3 * each, each + 2.0 * width_n3 *
+                         each])
     total_line = union(line_list)
     re = 0.0
     for each in total_line:
-        temp = get_error(np.mean(each), (each[1] - each[0]) * 0.25)
+        temp = get_error(np.mean(each), (each[1] - each[0]) * 0.25, wave, flux,
+                         error)
         re = re + temp ** 2.0
     return np.sqrt(re)
 
@@ -36,8 +42,13 @@ def estimate_error_single(rmid, hbetab_dic, hbetan_dic, o3_dic, fe2_dic, mjd):
                              "/" + str(mjd) + "/" + "error_scaled.pkl", "rb"))
     [wave, flux, error] = extract_fit_part(wave, flux, error, 4000.0, 5500.0)
     [wave, flux, error] = mask_points(wave, flux, error)
-    res = read_database(Location.project_loca + "result/fit_with_temp/data/" +
-                        str(rmid) + "/" + str(mjd) + "-Fe2.pkl")
+    print(error)
+    try:
+        res = read_database(Location.project_loca +
+                            "result/fit_with_temp/data/" +
+                            str(rmid) + "/" + str(mjd) + "-Fe2.pkl")
+    except OSError:
+        return
     fe_temp = np.vectorize(FeII_template_obs(res[0], res[1], res[2], res[3],
                                              res[4], res[5]))
     flux_fe = fe_temp(wave)
@@ -50,6 +61,10 @@ def estimate_error_single(rmid, hbetab_dic, hbetan_dic, o3_dic, fe2_dic, mjd):
     hbetan_dic[mjd] = hbetan_re
     o3_dic[mjd] = o3_re
     fe2_dic[mjd] = fe2_re
+    print(hbetab_dic)
+    print(hbetan_dic)
+    print(o3_dic)
+    print(fe2_dic)
 
 
 def error_estimate(rmid):
@@ -60,4 +75,13 @@ def error_estimate(rmid):
     o3 = m.dict()
     fe2 = m.dict()
     func = partial(estimate_error_single, rmid, hb, hn, o3, fe2)
+    mjd_list = map(int, os.listdir(Location.project_loca + "data/raw/" +
+                                   str(rmid)))
+    p.map(func, mjd_list)
 
+m = Manager()
+hb = m.dict()
+hn = m.dict()
+o3 = m.dict()
+fe2 = m.dict()
+estimate_error_single(1141, hb, hn, o3, fe2, 56660)
