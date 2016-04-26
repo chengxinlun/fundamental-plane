0981918f8443cf5f45f0dae19ecc67f77f4d0ca6
Xinlun Cheng <chengxinlun@gmail.com>
2016-04-26 21:33:31 +0800

    Start writing reverberation mapping program

--- rm.py
+++ rm.py
@@ -9,5 +9,15 @@ def read_flux(rmid, band):
     return dic
 
 
-def 
+def read_re(rmid, band):
+    f = open(Location.project_loca + "result/flux_of_line/" + str(rmid) + "/" + str(band) + "-error.pkl", "rb")
+    dic =pickle.load(f)
+    return dic
 
+
+def lc_gene(rmid):
+    band_list = ["hbeta", "o3", "cont"]
+    lc = dict()
+
+    for each in band_list:
+        lc[each] =
