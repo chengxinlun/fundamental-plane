19c9f894fc8f2440e0dfe92d6d9fe2c46fac5cec
chengxinlun <chengxinlun@gmail.com>
2016-05-01 11:45:59 +0800

    Lightcurve generating

--- rm.py
+++ rm.py
@@ -3,29 +3,36 @@ import os
 import pickle
 from position import Location
 
+
 def read_flux(rmid, band):
-    f = open(Location.project_loca + "result/flux_of_line/" + str(rmid) + "/" + str(band) + ".pkl", "rb")
+    f = open(Location.project_loca + "result/flux_of_line/" + str(rmid) + "/" +
+             str(band) + ".pkl", "rb")
     dic = pickle.load(f)
     return dic
 
 
 def read_re(rmid, band):
-    f = open(Location.project_loca + "result/flux_of_line/" + str(rmid) + "/" + str(band) + "-error.pkl", "rb")
-    dic =pickle.load(f)
+    f = open(Location.project_loca + "result/flux_of_line/" + str(rmid) + "/" +
+             str(band) + "-error.pkl", "rb")
+    dic = pickle.load(f)
     return dic
 
 
 def lc_gene(rmid):
     band_list = ["hbeta", "o3", "cont"]
     for each in band_list:
-        flux = read_flux(rmid, each)
-        error = read_re(rmid, each)
+        try:
+            flux = read_flux(rmid, each)
+            error = read_re(rmid, each)
+        except Exception:
+            return
         all_mjd = list()
         all_mjd.extend(flux.keys())
         all_mjd.extend(error.keys())
         mjd_list = sorted(set(all_mjd))
-        lc = list()
-        err = list()
+        lc_file = open(Location.project_loca + "result/light_curve/" +
+                       str(rmid) + "/" + str(each) + ".txt", "w")
         for each_day in mjd_list:
-            lc.append(flux[each])
-            err.append(flux[each] * error[each])
+            lc_file.write(str(each_day) + "    " + str(flux[each]) + "    " +
+                          str(flux[each] * error[each]))
+        lc_file.close()
