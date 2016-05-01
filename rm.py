73ee324d8ccdc8a40045133b77f2fda9e631ba65
Xinlun Cheng <chengxinlun@gmail.com>
2016-05-01 11:41:11 +0800

    1. Minor bug fix for fitting
    2. Bug fix for error estimation
    
    3. Lightcurve generating

--- rm.py
+++ rm.py
@@ -3,23 +3,30 @@ import os
 import pickle
 from position import Location
 
+
 def read_flux(rmid, band):
-    f = open(Location.project_loca + "result/flux_of_line/" + str(rmid) + "/" + str(band) + ".pkl", "rb")
+    f = open(Location.project_loca + "result/flux_of_line/" + str(rmid) + "/" +
+             str(band) + ".pkl", "rb")
     dic = pickle.load(f)
     return dic
 
 
 def read_re(rmid, band):
-    f = open(Location.project_loca + "result/flux_of_line/" + str(rmid) + "/" + str(band) + "-error.pkl", "rb")
-    dic =pickle.load(f)
+    f = open(Location.project_loca + "result/flux_of_line/" + str(rmid) + "/" +
+             str(band) + "-error.pkl", "rb")
+    dic = pickle.load(f)
     return dic
 
 
 def lc_gene(rmid):
     band_list = ["hbeta", "o3", "cont"]
     for each in band_list:
-        flux = read_flux(rmid, each)
-        error = read_re(rmid, each)
+        try:
+            flux = read_flux(rmid, each)
+            error = read_re(rmid, each)
+        except Exception:
+            print("Unable to locate flux or error file. Exiting.")
+            return
         all_mjd = list()
         all_mjd.extend(flux.keys())
         all_mjd.extend(error.keys())
@@ -29,3 +36,4 @@ def lc_gene(rmid):
         for each_day in mjd_list:
             lc.append(flux[each])
             err.append(flux[each] * error[each])
+
