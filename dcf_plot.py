7e5897a2cf4790dab492229e4ba842f877bd1b2f
chengxinlun <chengxinlun@gmail.com>
2016-08-09 17:54:20 +0800

    1. Added gitignore file
    2. Added maximum point extraction in plotting procedure

--- dcf_plot.py
+++ dcf_plot.py
@@ -5,6 +5,7 @@ from base import get_total_rmid_list, save_fig
 
 
 rmid_list = get_total_rmid_list()
+dcf_sum = Location.project_loca + "result/light_curve/dcf.txt"
 for each in rmid_list:
     try:
         dcf_file = Location.project_loca + "result/light_curve/" + str(each) + \
@@ -17,9 +18,18 @@ for each in rmid_list:
         dcf_m_err = dcf_data[:, 4]
         dcf_p_err = dcf_data[:, 5]
         fig = plt.figure()
-        plt.errorbar(t, dcf, yerr=[dcf_m_err, dcf_p_err], xerr=[t_m_sig, t_p_sig])
+        plt.errorbar(t, dcf, yerr=[dcf_m_err, dcf_p_err],
+                     xerr=[t_m_sig, t_p_sig])
         save_fig(fig, Location.project_loca + "result/light_curve/" + str(each),
                  "dcf")
         plt.close()
+        max_index = dcf.argmax()
+        t_max = t[max_index]
+        t_max_m_sig = t_m_sig[max_index]
+        t_max_p_sig = t_p_sig[max_index]
+        sum_file = open(dcf_sum, "a")
+        sum_file.write(str(each) + " " + str(t_max) + " " + str(t_max_m_sig) +
+                       " " + str(t_max_p_sig) + "\n")
+        sum_file.close()
     except Exception as reason:
         print(str(each) + " failed due to " + str(reason))
