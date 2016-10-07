85ca58f1ec95110608ec650d6eb07e34d8907fbd
chengxinlun <chengxinlun@gmail.com>
2016-10-07 21:40:09 +0800

    Add fitting.fitter module for fitter

---
+++ fitting/fitter.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+fitting.fitter
+==============
+A module for fitter
+
+Note: All functions here must follow the same parameters and same return
+'''
+import warnings
+from astropy.modeling import fitting
+
+
+__all__ = ['lmlsq']
+
+
+def lmlsq(model, x, y, e, maxi):
+    '''
+    lmlsq(model, x, y, initp, maxiter)
+    ==================================
+    Input: model: A astropy.modeling.models class for the model with initial set
+           x: A numpy array for x
+           y: A numpy array for y
+           e: A numpy array for error of y
+           maxi: An int for max number of iterations
+
+    Output: res: A same model with optimized parameters
+    '''
+    fit = fitting.LevMarLSQFitter()
+    # Report all warnings as exceptions
+    with warnings.catch_warnings():
+        warnings.filterwarnings('error')
+        res = fit(model, x, y, weights=e**(0.0-2.0), maxiter=maxi)
+    return res
