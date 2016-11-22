cd44ae53150cd7a610afed77296813d8d61dfb14
chengxinlun <chengxinlun@gmail.com>
2016-11-22 15:33:27 +0800

    Remove polynomial from continuum

--- fitting/cont.py
+++ fitting/cont.py
@@ -24,17 +24,8 @@ class ContSdss(Fittable1DModel):
     amplitude = Parameter()
     x_0 = Parameter()
     alpha = Parameter()
-    c_0 = Parameter()
-    c_1 = Parameter()
-    c_2 = Parameter()
-    c_3 = Parameter()
 
     @staticmethod
-    def evaluate(x, amplitude, x_0, alpha, c_0, c_1, c_2, c_3):
+    def evaluate(x, amplitude, x_0, alpha):
         pl = models.PowerLaw1D(amplitude, x_0, alpha, fixed={"x_0": True})
-        po = models.Polynomial1D(3)
-        po.c0 = c_0
-        po.c1 = c_1
-        po.c2 = c_2
-        po.c3 = c_3
-        return pl(x) + po(x)
+        return pl(x)
